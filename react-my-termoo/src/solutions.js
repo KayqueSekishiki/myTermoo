d7da7a2ee96b2ef71492c356da2578a067355fbf
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2022-08-06 10:19:56 -0300

    Arquivo Inicial, Limpo!

---
+++ react-my-termoo/src/solutions.js
@@ -0,0 +1,19 @@
+const solutions = [
+  { id: 1, word: "termo" },
+  { id: 2, word: "algoz" },
+  { id: 3, word: "plena" },
+  { id: 4, word: "afeto" },
+  { id: 5, word: "vigor" },
+  { id: 6, word: "fazer" },
+  { id: 7, word: "inato" },
+  { id: 8, word: "cores" },
+  { id: 9, word: "nobre" },
+  { id: 10, word: "justo" },
+  { id: 11, word: "honra" },
+  { id: 12, word: "anexo" },
+  { id: 13, word: "azedo" },
+  { id: 14, word: "lapso" },
+  { id: 15, word: "arara" },
+];
+
+export default solutions;
