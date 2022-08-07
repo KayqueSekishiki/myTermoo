658fc9b6bce5bcc6759f39a33ee14a996aaca718
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2022-08-06 23:20:34 -0300

    Congrats, Create

--- react-my-termoo/src/components/Wordle.js
+++ react-my-termoo/src/components/Wordle.js
@@ -10,12 +10,18 @@ export default function Wordle({ solution }) {
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
-    return () => window.removeEventListener("keyup", handleKeyup);
-  }, [handleKeyup]);
+    if (isCorrect) {
+      console.log("Parabens, imagine uma tela de vitoria aqui!");
+      window.removeEventListener("keyup", handleKeyup);
+    }
 
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect);
-  }, [guesses, turn, isCorrect]);
+    if (turn > 5) {
+      console.log("aaah, não foi dessa vez T.T");
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup, isCorrect, turn]);
 
   return (
     <div>
